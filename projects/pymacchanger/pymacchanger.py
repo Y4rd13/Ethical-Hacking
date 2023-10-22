cfb5a4108ee5a960ffa84c3f34073b959b437644
Y4rd13 <benjaserrau@gmail.com>
2023-10-21 22:52:10 -0400

    refactorize pymacchanger

--- projects/pymacchanger/pymacchanger.py
+++ projects/pymacchanger/pymacchanger.py
@@ -5,118 +5,143 @@ import subprocess
 
 class PyMAChanger:
     def __init__(self, interface):
-        """
-        Initialize the PyMAChanger object with the specified network interface.
-        
-        :param interface: Name of the network interface to work with.
-        """
-        with open('./data/oui.json', 'r') as f:
-            self.oui_data = json.load(f)
-
-        with open('./data/wireless.list', 'r') as f:
-            self.wireless_list_lines = f.readlines()
-
         self.interface = interface
-        self.original_mac = self._get_mac_details()
+        self.OUI_DATA, self.WIRELESS_LIST_LINES = self.load_data()
 
-    def _get_mac_details(self):
-        """
-        Private method to get the current MAC address of the specified network interface.
-        
-        :return: Current MAC address of the interface or None if not found.
-        """
+    @staticmethod
+    def load_data():
+        with open('./data/oui.json', 'r') as f:
+            OUI_DATA = json.load(f)
+    
+        with open('./data/wireless.list', 'r') as f:
+            WIRELESS_LIST_LINES = f.readlines()
+
+        return OUI_DATA, WIRELESS_LIST_LINES
+
+    def _run_command(self, *args):
         try:
-            result = subprocess.check_output(["ifconfig", self.interface])
-            result = result.decode("utf-8")
-            mac_address_search = re.search(r"(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)", result)
-            return mac_address_search.group(0) if mac_address_search else None
-        except:
+            result = subprocess.check_output(args)
+            return result.decode("utf-8")
+        except subprocess.CalledProcessError as e:
             return None
 
-    def get_current_mac(self):
-        """
-        Get the current MAC address of the specified network interface and its associated manufacturer.
-        
-        :return: A formatted string displaying the current MAC address and its associated manufacturer.
-        """
-        current_mac = self._get_mac_details()
-        if current_mac:
-            oui_part = current_mac.replace(":", "").upper()[:6]
-            manufacturer = self.oui_data.get(oui_part, "Unknown Manufacturer").replace("\n", " ")
-            return f"Current MAC: {current_mac} ({manufacturer})"
+    def _set_interface_mac(self, mac_address):
+        try:
+            subprocess.call(["sudo", "ifconfig", self.interface, "down"])
+            subprocess.call(["sudo", "ifconfig", self.interface, "hw", "ether", mac_address])
+            subprocess.call(["sudo", "ifconfig", self.interface, "up"])
+            return True
+        except Exception as e:
+            return False
+
+    def _extract_mac_details(self, raw_output):
+        mac_address_search = re.search(r"(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)", raw_output)
+        if mac_address_search:
+            mac = mac_address_search.group(0)
+            oui_part = mac.replace(":", "").upper()[:6]
+            manufacturer_details = self.OUI_DATA.get(oui_part, "Unknown Manufacturer\nUnknown Address").split("\n")
+            manufacturer_name = manufacturer_details[0]
+            manufacturer_address = " ".join(manufacturer_details[1:]).strip()
+            return {
+                'mac': mac,
+                'vendor': {
+                    'name': manufacturer_name,
+                    'address': manufacturer_address
+                }
+            }
+        return None
+
+    def _get_mac_details(self, mode="current"):
+        if mode == "current":
+            result = self._run_command("ifconfig", self.interface)
+        elif mode == "permanent":
+            result = self._run_command("ethtool", "-P", self.interface)
         else:
             return None
 
+        return self._extract_mac_details(result) if result else None
+
+    def _generate_random_mac_suffix(self):
+        return ":{:02x}:{:02x}:{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    def _generate_random_mac(self):
+        return ":".join("{:02x}".format(random.randint(0, 255)) for _ in range(6))
+
+    def get_current_mac(self):
+        mac_details = self._get_mac_details("current")
+        if mac_details:
+            current_mac = mac_details['mac']
+            manufacturer = f"{mac_details['vendor']['name']} {mac_details['vendor']['address']}"
+            return f"Current MAC: {current_mac} ({manufacturer})"
+        return None
+
     def show(self):
         """
-        Show the current MAC address of the specified network interface.
+        Retrieve the current and permanent MAC addresses for the network interface.
         
-        :return: Current MAC address of the interface.
+        :return: A dictionary containing the current and permanent MAC addresses and their associated manufacturers.
         """
-        return self.get_current_mac()
+        return {
+            'current': self._get_mac_details("current"),
+            'permanent': self._get_mac_details("permanent")
+        }
 
     def restore(self):
         """
-        Restore the MAC address of the specified network interface to its original value.
+        Restore the MAC address of the specified network interface to its original (permanent) value.
         
-        :return: True if successful, False otherwise.
+        :return: True if the restoration was successful, False otherwise.
         """
-        try:
-            subprocess.call(["sudo", "ifconfig", self.interface, "down"])
-            subprocess.call(["sudo", "ifconfig", self.interface, "hw", "ether", self.original_mac])
-            subprocess.call(["sudo", "ifconfig", self.interface, "up"])
-            return True
-        except:
-            return False
+        permanent_details = self._get_mac_details("permanent")
+        return self._set_interface_mac(permanent_details['mac']) if permanent_details else False
 
     def set(self, option):
         """
         Set the MAC address of the specified network interface based on the given option.
         
         Options:
-        1. Set random vendor MAC of the same kind
-        2. Set random vendor MAC of any kind
+        1. Set random vendor MAC of the same kind (same vendor prefix)
+        2. Set random vendor MAC of any kind (random vendor prefix)
         3. Set fully random MAC
         
         :param option: Integer value representing the MAC setting option (1, 2, or 3).
-        :return: A formatted string displaying the new MAC address and its associated manufacturer.
+        :return: Dictionary containing the new MAC address and its associated manufacturer or None if an error occurs.
         """
-        manufacturer = None
-        
+        mac_details = self._get_mac_details("current")
+        if not mac_details:
+            return None
+        current_mac = mac_details['mac']
+
         if option == 1:
-            current_mac = self._get_mac_details()
-            if current_mac:
-                oui_part = current_mac.replace(":", "").upper()[:6]
-                new_mac = ":".join([oui_part[i:i+2] for i in range(0, 6, 2)]) + ":{:02x}:{:02x}:{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-                manufacturer = self.oui_data.get(oui_part, "Unknown Manufacturer").replace("\n", " ")
+            oui_part = current_mac[:8]
+            new_mac = f"{oui_part}{self._generate_random_mac_suffix()}"
 
         elif option == 2:
-            oui_part = random.choice(list(self.oui_data.keys()))
-            new_mac = ":".join([oui_part[i:i+2] for i in range(0, len(oui_part), 2)])
-            new_mac += ":{:02x}:{:02x}:{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            manufacturer = self.oui_data.get(oui_part, "Unknown Manufacturer").replace("\n", " ")
+            oui_part = random.choice(list(self.OUI_DATA.keys()))
+            new_mac = f"{':'.join([oui_part[i:i+2] for i in range(0, 6, 2)])}{self._generate_random_mac_suffix()}"
 
         elif option == 3:
-            new_mac = "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(
-                random.randint(0, 255), random.randint(0, 255), random.randint(0, 255),
-                random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-            )
+            new_mac = self._generate_random_mac()
 
         else:
             return None
 
-        try:
-            subprocess.call(["sudo", "ifconfig", self.interface, "down"])
-            subprocess.call(["sudo", "ifconfig", self.interface, "hw", "ether", new_mac])
-            subprocess.call(["sudo", "ifconfig", self.interface, "up"])
-            
-            # Format the output as requested
-            formatted_output = f"Current MAC: {new_mac} ({manufacturer})"
-            return formatted_output
-        except:
-            return None
+        success = self._set_interface_mac(new_mac)
+        return self.show() if success else None
+
+
 
 pymac = PyMAChanger("eth0")
-print(pymac.get_current_mac())
 print('----')
-print(pymac.set(option=1))
+print('SHOW')
+print(pymac.show())
+print('----')
+print('SET')
+option = 3
+print(f'option: {option}')
+print(pymac.set(option))
+print('----')
+pymac.restore()
+print('RESTORE')
+print(pymac.show())
+print('----')
